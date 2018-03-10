99e36f74cf4e52cc816a4d959c76ec14b81e2e7b
ncheneweth <nchenewe@thoughtworks.com>
2018-03-09 18:37:10 -0600

    testing new config

--- setup_kubernetes.sh
+++ setup_kubernetes.sh
@@ -4,10 +4,10 @@ export KOPS_STATE_STORE=s3://$1-$2-state
 export K8_CLUSTER_NAME=k8.$2.$3
 
 if [[ ! $(kops get cluster --name $K8_CLUSTER_NAME) ]]; then
-    kops create -f $2_cluster.yaml
+    kops create -f $2_cluster.yml
     kops create secret --name $K8_CLUSTER_NAME sshpublickey admin -i $2.id_rsa.pub
 else
-    kops replace -f $2_cluster.yaml
+    kops replace -f $2_cluster.yml
 fi
 
 kops update cluster $K8_CLUSTER_NAME --yes
